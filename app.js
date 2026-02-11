0d68ad71e2a5f2a3bf9466d35b91ca9d318a3765
bilalakmal-2004 <ghulambeast468@gmail.com>
2026-02-12 00:18:53 +0500

    Cart functions added.

--- app.js
+++ app.js
@@ -65,3 +65,83 @@ let person = {
 };
 person.widrawMoney(200);
 console.log(person.money);
+
+// some common string manipulation functions are
+let str = "Bilal";
+
+console.log(str.toUpperCase()); //BILAL
+
+console.log(str.split("").reverse().join("")); //laliB
+
+console.log(str.charAt(0).toLocaleLowerCase() + str.slice(1)); //bilal
+
+console.log(str.includes("i")); // true yes i exists
+
+console.log(str.replaceAll("ilal", "ILAL")); // BILAL save it to some vairble to use it
+
+console.log(str.repeat(2)); //BilalBilal
+
+console.log(str.slice(0, 3)); // Bil
+
+let str2 = "Kaleem Shakeel";
+let arr = str2.split(" ");
+console.log(arr); // [ 'Kaleem', 'Shakeel' ]
+
+console.log(arr.join(" ")); // Kaleem Shakeel
+
+// here are the function for the cart system
+
+let cart = [];
+
+// to add the some products in the cart
+let watch = {
+  id: 1,
+  model: "apple",
+  price: 200,
+  quantity: 1,
+};
+function addToCart(product) {
+  let existing = cart.find((item) => item.id === product.id);
+
+  if (existing) {
+    existing.quantity++;
+  } else {
+    cart.push({ ...product, quantity: 1 });
+  }
+}
+
+addToCart(watch);
+console.log(cart);
+
+// if user want to add the aadd the same product again he will click the plus button
+function updateQuantity(productId, newQuantity) {
+  let item = cart.find((item) => item.id === productId);
+  if (item) {
+    item.quantity = newQuantity;
+  }
+}
+
+updateQuantity(watch.id, 3);
+console.log(cart);
+
+//to remve  item fom the cart
+function removeFromCart(productId) {
+  cart = cart.filter((item) => item.id !== productId);
+}
+
+// removeFromCart(watch.id);
+console.log(cart);
+
+// to calculate the total
+function getTotal() {
+  return cart.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+}
+
+console.log(getTotal());
+
+//remove full cart
+function clearCart() {
+  cart = [];
+}
